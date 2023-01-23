2ef359c85a04c9485cd8e659aecf9650c6d40697
Rishita Shaw <rishitashaw49@gmail.com>
2023-01-23 21:43:42 +0530

    user model modified

--- mapayl/users/models.py
+++ mapayl/users/models.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
@@ -52,6 +53,16 @@ class User(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
+    KYC_STATUS = (
+        ('unverified', _('Unverified')),
+        ('verified', _('Verified')),
+        ('pending', _('Pending')),
+        ('action_required', _('Action Required')),
+        ('cancelled', _('Cancelled')),
+        ('failed', _('Failed')),
+        ('rejected', _('Rejected')),
+    )
+
     id = models.UUIDField(
         primary_key=True,
         default=uuid.uuid4,
@@ -76,6 +87,124 @@ class User(AbstractUser):
         max_length=125,
         unique=True,
         help_text=_("Email address of the client."))
+
+    current_address = models.CharField(
+        verbose_name=_('current address'),
+        max_length=1000,
+        blank=True, null=True,
+        help_text=_("The current living address of the borrower. Verification is required"))
+
+    permanent_address = models.CharField(
+        verbose_name=_('permanent address'),
+        max_length=1000,
+        blank=True, null=True,
+        help_text=_("Permanent living address of the borrower. Verification and privacy rules applies"))
+
+    contact_number = models.CharField(
+        verbose_name=_('contact number'),
+        max_length=20,
+        blank=True, null=True,
+        help_text=_("International format of borrower's contact number. Phone number if provided must be unique"))
+
+    date_of_birth = models.DateField(
+        verbose_name=_('date of birth'),
+        blank=True, null=True,
+        help_text=_("Date of birth of borrower. Registered borrowers must be not less than 18 years of age "))
+
+    kyc_complete = models.BooleanField(
+        verbose_name=_('kyc_complete'),
+        default=False,
+        help_text=_("Indication if borrower has completing KYC verification processes"))
+
+    kyc_complete_date = models.DateTimeField(
+        verbose_name=_('date of kyc completion'),
+        blank=True, null=True,
+        help_text=_("KYC complete date"))
+
+    kyc_status = models.CharField(
+        verbose_name=_('kyc status'),
+        choices=KYC_STATUS,
+        default="unverified",
+        max_length=20,
+        help_text=_("kyc status of the client."))
+
+    on_boarding_complete = models.BooleanField(
+        verbose_name=_('on boarding status'),
+        default=False,
+        help_text=_("on boarding status of the client."))
+
+    on_boarding_complete_date = models.DateTimeField(
+        verbose_name=_('date of on_boarding completion'),
+        blank=True, null=True,
+        help_text=_("on boarding complete date"))
+
+    kyc_submitted = models.BooleanField(
+        verbose_name=_('kyc submitted'),
+        default=False,
+        help_text=_("Indication if borrower has submitted KYC verification processes"))
+
+    aadhar_number = models.CharField(
+        verbose_name=_('aadhar number'),
+        max_length=20,
+        blank=True, null=True,
+        help_text=_("AAdhar number. Must be unique"))
+
+    place_of_birth = models.CharField(
+        verbose_name=_('place of birth'),
+        max_length=125,
+        blank=True, null=True,
+        help_text=_("The birth place of borrower"))
+
+    verification_date = models.DateTimeField(
+        verbose_name=_('verification date'),
+        default=timezone.now,
+        blank=True, null=True,
+        help_text=_("The verification date of borrower"))
+
+    registered_ip_address = models.CharField(
+        verbose_name=_('registered ip address'),
+        max_length=125,
+        blank=True, null=True,
+        help_text=_("The registered ip address of client"))
+
+    country_of_residence = models.CharField(
+        verbose_name=_('country of residence'),
+        max_length=125,
+        blank=True, null=True,
+        help_text=_("The country of residence of borrower"))
+
+    job_title = models.CharField(
+        verbose_name=_('job title'),
+        max_length=125,
+        blank=True, null=True,
+        help_text=_("The job title of borrower"))
+    default_currency = models.CharField(
+        verbose_name=_('default currency'),
+        max_length=3,
+        default='INR',
+        blank=True, null=True,
+        help_text=_("The default currency of borrower"))
+
+    ################################################################
+    ################ pending models ################################
+    ################################################################
+
+    # pending_cash_balance
+    # time_zone
+    # salutation
+    # highest_qualification
+    # passout_year
+    # investment_limit
+    # fund_committed
+    # escrow_account_number
+    # tax_id
+
+    class Meta:
+        verbose_name = _("Registered User")
+        verbose_name_plural = _("Registered Users")
+
+    def __str__(self):
+        return self.email
 
     def get_absolute_url(self):
         """Get url for user's detail view.
