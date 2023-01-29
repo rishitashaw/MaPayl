f114b4227fc057d5d814ff6cd1376c418537c307
Rishita Shaw <rishitashaw49@gmail.com>
2023-01-30 03:26:26 +0530

    css

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('/', views.index, name='home'),
+    path('', views.index, name='home'),
     path('borrow/', views.borrow, name='borrow'),
     path('invest/', views.invest, name='invest'),
     path('about-us/', views.about_us, name='about'),
